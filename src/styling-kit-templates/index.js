50e08c0b16f352894b45d2b247793b66be4fc53a
4M8B <Manu.bertrand@protonmail.com>
2022-06-10 17:12:55 +0200

    Partial Commit -- Updated

---
+++ src/styling-kit-templates/index.js
@@ -0,0 +1,33 @@
+import { GlobalStyle } from './GlobalStyle'
+
+import Accordion from './Components/Accordion'
+import AccordionGroup  from './Components/AccordionGroup'
+import  Alert from './Components/Alert'
+import  Badge from './Components/Badge'
+import  Breadcrumb from './Components/BreadCrumb'
+import  BreadcrumbItem from './Components/BreadCrumbItem'
+import  Button from './Components/Button'
+import  ButtonDropdown from './Components/ButtonDropdown'
+import  ButtonGroup from './Components/ButtonGroup'
+import  ButtonToolbar from './Components/ButtonToolbar'
+import  Card from './Components/Card'
+import  CardBlock from './Components/CardBlock'
+import  CardBlockquote from './Components/CardBlockquote'
+import  CardColumns from './Components/CardColumns'
+import  CardDeck from './Components/CardDeck'
+import  CardFooter from './Components/CardFooter'
+import  CardGroup from './Components/CardGroup'
+import  CardHeader from './Components/CardHeader'
+import  CardImg from './Components/CardImg'
+import  CardImgOverlay from './Components/CardImgOverlay'
+import  CardLink from './Components/CardLink'
+import  CardSubtitle from './Components/CardSubtitle'
+import  CardText from './Components/CardText'
+import  CardTitle from './Components/CardTitle'
+import  Collapse from './Components/Collapse'
+import  Drawer from './Components/Drawer'
+
+
+export {
+
+}
