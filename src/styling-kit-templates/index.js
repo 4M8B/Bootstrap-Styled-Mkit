e485bdfdb7f2832b9783bd7fbeef9be5fb8eae5f
4M8B <Manu.bertrand@protonmail.com>
2022-06-11 15:19:25 +0200

    Partial Commit -- Updated

--- src/styling-kit-templates/index.js
+++ src/styling-kit-templates/index.js
@@ -1,7 +1,7 @@
 import { GlobalStyle } from './GlobalStyle'
 
-import Accordion from './Components/Accordion'
-import AccordionGroup  from './Components/AccordionGroup'
+import  Accordion from './Components/Accordion'
+import  AccordionGroup  from './Components/AccordionGroup'
 import  Alert from './Components/Alert'
 import  Badge from './Components/Badge'
 import  Breadcrumb from './Components/BreadCrumb'
@@ -26,8 +26,216 @@ import  CardText from './Components/CardText'
 import  CardTitle from './Components/CardTitle'
 import  Collapse from './Components/Collapse'
 import  Drawer from './Components/Drawer'
+import  Dropdown from './Components/Dropdown'
+import  DropdownItem from './Components/DropdownItem'
+import  DropdownMenu from './Components/DropdownMenu'
+import  DropdownToggle from './Components/DropdownToggle'
+import  Form from './Components/Form'
+import  FormCustom from './Components/FormCustom'
+import  FormFeedback from './Components/FormFeedback'
+import  FormGroup from './Components/FormGroup'
+import  FormText from './Components/FormText'
+import  InputGroup from './Components/InputGroup'
+import  InputputGroupAddon from './Components/InputGroupAddon'
+import  InputGroupButton from './Components/InputGroupButton'
+import  Jumbotron from './Components/Jumbotron'
+import  ListGroup from './Components/ListGroup'
+import  ListGroupItem from './Components/ListGroupItem'
+import  ListGroupItemHeading from './Components/ListGroupItemHeading'
+import  ListGroupItemText from './Components/ListGroupItemText'
+import  Media from './Components/Media'
+import  Modal from './Components/Modal'
+import  ModalBody from './Components/ModalBody'
+import  ModalFooter from './Components/ModalFooter'
+import  ModalHeader from './Components/ModalHeader'
+import  Nav from './Components/Nav'
+import  Navbar from './Components/Navbar'
+import  NavbarBrand from './Components/NavbarBrand'
+import  NavbarToggler from './Components/NavbarToggler'
+import  NavDropdown from './Components/NavDropdown'
+import  NavItem from './Components/NavItem'
+import  NavLink from './Components/NavLink'
+import  Pagination from './Components/Pagination'
+import  PaginationItem from './Components/PaginationItem'
+import  Progress from './Components/Progress'
+import  ProgressBar from './Components/ProgressBar'
+import  Table from './Components/Table'
+import  Tbody from './Components/Tbody'
+import  Td from './Components/Td'
+import  Tfoot from './Components/Tfoot'
+import  Th from './Components/Th'
+import  Thead from './Components/Thead'
+import  Tooltip from './Components/Tooltip'
+import  Tr from './Components/Tr'
 
+import  A from './Content/A'
+import  Abbr from './Content/Abbr'
+import  Address from './Content/Address'
+import  Area from './Content/Area'
+import  Blockquote from './Content/Blockquote'
+import  Caption from './Content/Caption'
+import  Close from './Content/Close'
+import  Code from './Content/Code'
+import  Dd from './Content/Dd'
+import  Details from './Content/Details'
+import  Dfn from './Content/Dfn'
+import  Dl from './Content/Dl'
+import  Dt from './Content/Dt'
+import  Fieldset from './Content/Fieldset'
+import  H1 from './Content/H1'
+import  H2 from './Content/H2'
+import  H3 from './Content/H3'
+import  H4 from './Content/H4'
+import  H5 from './Content/H5'
+import  H6 from './Content/H6'
+import  Header from './Content/Header'
+import  Hr from './Content/Hr'
+import  Image from './Content/Image'
+import  Input from './Content/Input'
+import  IssueIcon from './Content/IssueIcon'
+import  Kbd from './Content/Kbd'
+import  Legend from './Content/Legend'
+import  Li from './Content/Li'
+import  Map from './Content/Map'
+import  Mark from './Content/Mark'
+import  Ol from './Content/Ol'
+import  P from './Content/P'
+import  Pre from './Content/Pre'
+import  Samp from './Content/Samp'
+import  Select from './Content/Select'
+import  Small from './Content/Small'
+import  Strong from './Content/Strong'
+import  Sub from './Content/Sub'
+import  Summary from './Content/Summary'
+import  Sup from './Content/Sup'
+import  TextArea from './Content/TextArea'
+import  Ul from './Content/Ul'
+
+import Col from './Layout/Col'
+import Container from './Layout/Container'
+import Row from './Layout/Row'
 
 export {
+    GlobalStyle,
+}
 
+export {
+    Accordion,
+    AccordionGroup,
+    Alert,
+    Badge,
+    Breadcrumb,
+    BreadcrumbItem,
+    Button,
+    ButtonDropdown,
+    ButtonGroup,
+    ButtonToolbar,
+    Card,
+    CardBlock,
+    CardBlockquote,
+    CardColumns,
+    CardDeck,
+    CardFooter,
+    CardGroup,
+    CardHeader,
+    CardImg,
+    CardImgOverlay,
+    CardLink,
+    CardSubtitle,
+    CardText,
+    CardTitle,
+    Collapse,
+    Drawer,
+    Dropdown,
+    DropdownItem,
+    DropdownMenu,
+    DropdownToggle,
+    Form,
+    FormCustom,
+    FormFeedback,
+    FormGroup,
+    FormText,
+    InputGroup,
+    InputputGroupAddon,
+    InputGroupButton,
+    Jumbotron,
+    ListGroup,
+    ListGroupItem,
+    ListGroupItemHeading,
+    ListGroupItemText,
+    Media,
+    Modal,
+    ModalBody,
+    ModalFooter,
+    ModalHeader,
+    Nav,
+    Navbar,
+    NavbarBrand,
+    NavbarToggler,
+    NavDropdown,
+    NavItem,
+    NavLink,
+    Pagination,
+    PaginationItem,
+    Progress,
+    ProgressBar,
+    Table,
+    Tbody,
+    Td,
+    Tfoot,
+    Th,
+    Thead,
+    Tooltip,
+    Tr,
+}
+
+export {
+    A,
+    Abbr,
+    Address,
+    Area,
+    Blockquote,
+    Caption,
+    Close,
+    Code,
+    Dd,
+    Details,
+    Dfn,
+    Dl,
+    Dt,
+    Fieldset,
+    H1,
+    H2,
+    H3,
+    H4,
+    H5,
+    H6,
+    Header,
+    Hr,
+    Image,
+    Input,
+    IssueIcon,
+    Kbd,
+    Legend,
+    Li,
+    Map,
+    Mark,
+    Ol,
+    P,
+    Pre,
+    Samp,
+    Select,
+    Small,
+    Strong,
+    Sub,
+    Summary,
+    Sup,
+    TextArea,
+    Ul,
+}
+
+export {
+    Col,
+    Container,
+    Row,
 }
